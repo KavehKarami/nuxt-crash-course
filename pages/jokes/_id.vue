146001c380293218448b181340a4b7d3b433b8c3
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-02 21:51:01 +0430

    dynamic routes

---
+++ pages/jokes/_id.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1 class="text-2xl mb-3 font-bold">jokeID : {{$route.params.id}}</h1>
+    <p>{{joke.joke}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleJokes",
+
+  async asyncData ({ $axios , params }) {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    }
+    const joke = await $axios.$get(`https://icanhazdadjoke.com/j/${params.id}`,config);
+    return { joke }
+  },
+
+  data() {
+    return {
+      joke: {}
+    }
+  },
+
+  head(){
+    return {
+      title: `jokeID : ${this.$route.params.id}`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    }
+  },
+}
+
+  
+</script>
+
+<style lang="scss" scoped>
+
+</style>
