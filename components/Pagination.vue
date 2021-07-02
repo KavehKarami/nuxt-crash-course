250663777bda60331a0e2b6135e7d3dfb499664b
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-02 23:24:26 +0430

    refactor Pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-3">
+    <button :class="'h-11 mr-4 bg-blue-700 focus:bg-blue-600 py-2 px-3 rounded shadow-sm focus:shadow-md transition duration-500 ease-in-out text-white ' + (currentPage === 1 && ' opacity-50')" :disabled="currentPage === 1" @click="handlePage(currentPage - 1)">
+      prev
+    </button>
+    <span>current: <span class="font-bold">{{currentPage}}</span></span>
+    <button :class="'h-11 ml-4 bg-indigo-700 focus:bg-indigo-600 py-2 px-3 rounded shadow-sm focus:shadow-md transition duration-500 ease-in-out text-white ' + (totalPage === currentPage && ' opacity-50')" :disabled="totalPage === currentPage" @click="handlePage(nextPage)">
+      next page
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Pagination",
+
+    props: {
+      currentPage: {
+        type: Number,
+        default: 1,
+      },
+      totalPage: {
+        type: Number,
+        default: 1,
+      },
+      nextPage: {
+        type: Number,
+        default: 1,
+      },
+    },
+
+    methods: {
+      handlePage(page) {
+        this.$emit("handle-pagination", page)
+      }
+    },
+  }
+</script>
--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,16 +1,8 @@
 <template>
   <div>
     <SearchJokes @search-joke="handleSearch" />
-    
-    <div class="my-3">
-      <button :class="'h-11 mr-4 bg-blue-700 focus:bg-blue-600 py-2 px-3 rounded shadow-sm focus:shadow-md transition duration-500 ease-in-out text-white ' + (currentPage === 1 && ' opacity-50')" :disabled="currentPage === 1" @click="handlePage(currentPage - 1)">
-        prev
-      </button>
-      <span>current: <span class="font-bold">{{currentPage}}</span></span>
-      <button :class="'h-11 ml-4 bg-indigo-700 focus:bg-indigo-600 py-2 px-3 rounded shadow-sm focus:shadow-md transition duration-500 ease-in-out text-white ' + (totalPage === currentPage && ' opacity-50')" :disabled="totalPage === currentPage" @click="handlePage(nextPage)">
-        next page
-      </button>
-    </div>
+
+    <Pagination :next-page="nextPage" :total-page="totalPage" :current-page="currentPage" @handle-pagination="handlePage" />
     
     <Joke v-for="joke of jokes" :id="joke.id" :key="joke.id" :joke="joke.joke" />
   </div>
@@ -74,14 +66,14 @@ export default {
       }
     },
 
-    async handlePage(count) {
+    async handlePage(page) {
       const config = {
         headers: {
           Accept: "application/json"
         }
       }
       try {
-        const res = await axios.get(`https://icanhazdadjoke.com/search?term=&page=${count}`, config);
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term$=&page=${page}`, config);
 
         const {results:jokes,total_pages:totalPage, next_page: nextPage, current_page:currentPage} = res.data;
 
