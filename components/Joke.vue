59cdd8db8000a13dd0c86226a03431ecfc9aa620
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-02 21:09:00 +0430

    get jokes from api

---
+++ components/Joke.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="joke">
+    <p>{{joke}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Joke",
+    props: {
+      joke: {
+        type: String,
+        default: ""
+      },
+      id: {
+        type: String,
+        default: ""
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.joke {
+  padding: 1rem;
+  border: 1px dashed #ccc;
+  margin: 1rem 0;
+}
+</style>
--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
-    jokes
+    <Joke v-for="joke of jokes" :id="joke.id" :key="joke.id" :joke="joke.joke" />
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import axios from "axios";
+
+export default {
+  name: "Jokes",
+
+  data() {
+    return {
+      jokes: []
+    }
+  },
+  
+  head(){
+    return {
+      title: "dad jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "best place for corny dad jokes"
+        }
+      ]
+    }
+  },
+
+  async mounted () {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    }
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (err) {
+      throw new Error(err)
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
